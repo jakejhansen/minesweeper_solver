a1c82b4add99e6c834c08bf8ba6e358855d5cab0
Jacob Jon Hansen <jacobjonhansen@gmail.com>
2017-10-18 04:28:19 +0200

    Moved old human playable to minesweeper_old. Wrapped minesweeper into a class which can be imported and used by an agent, taking a action on the object and having a state and reward returned

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,94 +1,279 @@
 import pygame
 import math
+import numpy as np
+import random
+import time
 
-def checkpattern(col, row):
-    if row % 2:
-        if col % 2: #If unequal
-            return True
-        else: #if equal
-            return False
-    else: 
-        if col % 2: #If unequal
-            return False
-        else: #if equal
-            return True
+class Minesweeper(object):
+    def __init__(self, ROWS = 10, COLS = 10, SIZEOFSQ = 100, MINES = 13, display = False):
+        """ Initialize Minesweeper
+            Rows, Cols: int  - Number of rows and cols on the board
+            SIZEOFSQ: pixels -  Determines the size of the window, reduce to get smaller window
+            Mines: integer - Number of mines generated on the board
+            display: bool - chooses weather to display the game with pygame
+        """
 
-def drawSpirit(screen, col, row, type, myfont):
-    """
-    Draws a spirit at pos col, row of type = [blank, bomb, one, two, three, four, five, six]
-    """
-    if type == 'blank':
-        if checkpattern(col,row):
-            c = (242, 244, 247)
+        self.ROWS = ROWS
+        self.COLS = COLS
+        self.SIZEOFSQ = SIZEOFSQ
+        self.MINES = MINES
+        self.display = display
+
+        self.grid = np.zeros((self.ROWS, self.COLS), dtype=object)
+        self.state = np.zeros((self.ROWS, self.COLS), dtype=object)
+        self.state_last = np.copy(self.state)
+
+
+        if display: #Load pygame stuff
+            pygame.init()
+            pygame.font.init()
+
+            self.myfont = pygame.font.SysFont("monospace-bold", 100)
+            self.screen = pygame.display.set_mode((COLS * SIZEOFSQ, ROWS * SIZEOFSQ))
+
+        self.initGame()
+
+
+    def initGame(self):
+        if self.display:
+            self.initPattern()
+        self.grid = self.initBoard(startcol = 5, startrow = 5)
+        self.state = np.ones((self.ROWS, self.COLS), dtype=object) * 'U'
+        self.state_last = np.copy(self.state)
+
+
+        self.action(5,5) #Hack alert, to start off with non empty board. Can be removed but then agent has to learn
+                         #what to do when the board starts out empty. 
+
+    def initBoard(self, startcol, startrow):
+        """ Initializes the board """
+
+        COLS = self.COLS
+        ROWS = self.ROWS
+        grid = self.grid.copy()
+        mines = self.MINES
+
+        #Randomly place bombs
+        while mines > 0:
+            (row, col) = (random.randint(0, ROWS-1), random.randint(0, COLS-1))
+            #if (col,row) not in findNeighbors(startcol, startrow, grid) and grid[col][row] != 'B' and (col, row) not in (startcol, startrow):
+            if (row,col) not in self.findNeighbors(startrow, startcol) and (row,col) != (startrow, startcol) and grid[row][col] != 'B':
+                grid[row][col] = 'B'
+                mines = mines - 1
+
+
+        #Get rest of board when bombs have been placed
+        for col in range(COLS):
+            for row in range(ROWS):
+                if grid[row][col] != 'B':
+                    totMines = self.sumMines(col, row, grid)
+                    if totMines > 0:
+                        grid[row][col] = totMines
+                    else:
+                        grid[row][col] = 'E'
+
+
+        return grid
+
+
+    def findNeighbors(self, rowin, colin):
+        """ Takes col, row and grid as input and returns as list of neighbors
+        """
+        COLS = self.grid.shape[1]
+        ROWS = self.grid.shape[0]
+        neighbors = []
+        for col in range(colin-1, colin+2):
+            for row in range(rowin-1, rowin+2):
+                if (-1 < rowin < ROWS and 
+                    -1 < colin < COLS and 
+                    (rowin != row or colin != col) and
+                    (0 <= col < COLS) and
+                    (0 <= row < ROWS)):
+                    neighbors.append((row,col))
+
+        return neighbors
+
+
+    def sumMines(self, col, row, grid):
+        """ Finds amount of mines adjacent to a field.
+        """
+        mines = 0
+        neighbors = self.findNeighbors(row, col)
+        for n in neighbors:
+            if grid[n[0],n[1]] == 'B':
+                mines = mines + 1
+        return mines
+
+
+    def printState(self):
+        """Prints the current state"""
+        grid = self.state
+        COLS = grid.shape[1]
+        ROWS = grid.shape[0]
+        for row in range(0,ROWS):
+            print(' ')
+            for col in range(0,COLS):
+                print(grid[row][col], end=' ')
+
+
+    def printBoard(self):
+        """Prints the board """
+        grid = self.grid
+        COLS = grid.shape[1]
+        ROWS = grid.shape[0]
+        for row in range(0,ROWS):
+            print(' ')
+            for col in range(0,COLS):
+                print(grid[row][col], end=' ')
+
+
+    def reveal(self, col, row, checked, press = "LM"):
+        """Finds out which values to show in the state when a square is pressed
+           Checked : np.array((row,col)) to check which squares has already been checked
+                     If the field is not a bomb we want to reveal it, if the field is empty
+                     we want to find it's neighbors and reveal them too if they are not a bomb.   
+        """
+        if press == "LM":
+            if checked[row][col] != 0:
+                return
+            checked[row][col] = checked[row][col] + 1
+            if self.grid[row][col] != 'B':
+
+                if self.display:
+                    self.drawSpirit(col, row, self.grid[row][col])
+
+                #Reveal to state space
+                self.state[row][col] = self.grid[row][col]
+
+                if self.grid[row][col] == 'E':
+                    neighbors = self.findNeighbors(row, col)
+                    for n in neighbors:
+                        if not checked[n[0],n[1]]: 
+                            self.reveal(n[1], n[0], checked)
+        
+        elif press == "RM":
+            #Draw flag, not used for agent
+            pass
+
+            if self.display:
+                self.drawSpirit(col, row, "flag")
+
+
+
+    def action(self, row, col):
+        """ External action, taken by human or agent
+            row,col: integer - where the agent want to press
+         """
+
+        #If press a bomb game over, start new game and return bad reward, -10 in this case
+        if self.grid[row][col] == "B":
+            self.initGame()
+            return({"s" : self.state, "r" : -10})
+
+        #Take action and reveal new state
+        self.reveal(col, row , np.zeros_like(self.grid))
+        if self.display == True:
+            pygame.display.flip()
+
+        #Get the reward for the given action
+        reward = self.compute_reward()
+
+        #return the state and the reward
+        return({"s" : self.state, "r" : reward})
+
+    def compute_reward(self):
+        """Computes the reward for a given action"""
+
+        #Reward = 1 if we get less unknowns, 0 otherwise 
+        if (np.sum(self.state_last == 'U') - np.sum(self.state == 'U')) > 0:
+            reward = 1
         else:
-            c = (247, 249, 252)
+            reward = 0
 
-        pygame.draw.rect(screen, c, pygame.Rect(col*SIZEOFSQ, row*SIZEOFSQ, SIZEOFSQ, SIZEOFSQ))
 
-    else:
-        drawSpirit(screen, col, row, 'blank', myfont)
-        if type == 'one':
-            text = myfont.render("1", 1, (0, 204, 0))
-        elif type == 'two':
-            text = myfont.render("2", 1, (255, 204, 0))
-        elif type == 'three':
-            text = myfont.render("3", 1, (204, 0, 0))
-        elif type == 'four':
-            text = myfont.render("4", 1, (0, 51, 153))
-        elif type == 'five':
-            text = myfont.render("5", 1, (255, 102, 0))
-        elif type == 'six':
-            text = myfont.render("6", 1, (255, 102, 0))
-        elif type == 'flag':
-            text = myfont.render("F", 1, (255, 0, 0))
+        self.state_last = np.copy(self.state)
+        return(reward)
 
-        #Get the text rectangle and center it inside the rectangles
-        textRect = text.get_rect()
-        textRect.center = (col*SIZEOFSQ + int(0.5*SIZEOFSQ)),(row*SIZEOFSQ + int(0.5*SIZEOFSQ))
-        screen.blit(text, textRect)
+
+    def drawSpirit(self, col, row, type):
+        """
+        Draws a spirit at pos col, row of type = [E (empty), B (bomb), 1, 2, 3, 4, 5, 6, F (flag)]
+        """
+        myfont = self.myfont
+        SIZEOFSQ = self.SIZEOFSQ
+
+        if type == 'E':
+            if self.checkpattern(col,row):
+                c = (242, 244, 247)
+            else:
+                c = (247, 249, 252)
+            pygame.draw.rect(self.screen, c, pygame.Rect(col*SIZEOFSQ, row*SIZEOFSQ, SIZEOFSQ, SIZEOFSQ))
+
+        else:
+            self.drawSpirit(col, row, 'E')
+            if type == 1:
+                text = myfont.render("1", 1, (0, 204, 0))
+            elif type == 2:
+                text = myfont.render("2", 1, (255, 204, 0))
+            elif type == 3:
+                text = myfont.render("3", 1, (204, 0, 0))
+            elif type == 4:
+                text = myfont.render("4", 1, (0, 51, 153))
+            elif type == 5:
+                text = myfont.render("5", 1, (255, 102, 0))
+            elif type == 6:
+                text = myfont.render("6", 1, (255, 102, 0))
+            elif type == 'flag':
+                text = myfont.render("F", 1, (255, 0, 0))
+
+            #Get the text rectangle and center it inside the rectangles
+            textRect = text.get_rect()
+            textRect.center = (col*self.SIZEOFSQ + int(0.5*self.SIZEOFSQ)),(row*self.SIZEOFSQ + int(0.5*self.SIZEOFSQ))
+            self.screen.blit(text, textRect)
+            
+
+
+    def checkpattern(self, col, row):
+        #Function to construct the checked pattern in pygame
+        if row % 2:
+            if col % 2: #If unequal
+                return True
+            else: #if equal
+                return False
+        else: 
+            if col % 2: #If unequal
+                return False
+            else: #if equal
+                return True
+
+    def initPattern(self):
+        #Initialize pattern:
+
+        c1 = (4, 133, 223) #color1
+        c2 = (4, 145, 223) #color2
+        rects = []
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.checkpattern(col, row):
+                    c = c1
+                else:
+                    c = c2
+
+                rects.append(pygame.draw.rect(self.screen, c, pygame.Rect(col*self.SIZEOFSQ, row*self.SIZEOFSQ, self.SIZEOFSQ, self.SIZEOFSQ)))
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
 
-    ROWS = 10
-    COLS = 10
-    SIZEOFSQ = 100
+    game = Minesweeper(display=True)
+    game.printState()
 
-    #color of squares
-    c1 = (4, 133, 223)
-    c2 = (4, 145, 223)
-
-
-    pygame.init()
-    pygame.font.init()
-    myfont = pygame.font.SysFont("monospace-bold", 100)
-    screen = pygame.display.set_mode((COLS * SIZEOFSQ, ROWS * SIZEOFSQ))
-
-    rects = []
-
-    #Initialize Game:
-    for row in range(ROWS):
-        for col in range(COLS):
-            if checkpattern(col, row):
-                c = c1
-            else:
-                c = c2
-
-            rects.append(pygame.draw.rect(screen, c, pygame.Rect(col*SIZEOFSQ, row*SIZEOFSQ, SIZEOFSQ, SIZEOFSQ)))
+    while True:
+        inp = input("Enter input (ROW,COL)")
+        row = int(inp[1])
+        col = int(inp[3])
+        v = game.action(row, col)
+        game.printState()
+        print("\nReward = {}".format(v["r"]))
 
 
-    done = False
-    while not done:
-            for event in pygame.event.get():
-                #pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    pos = pygame.mouse.get_pos()
-                    for i, rect in enumerate(rects):
-                        if rect.collidepoint(pos):
-                            print(i)
-                            col = i % COLS
-                            row = math.floor(i/ROWS)
-                            drawSpirit(screen, col, row, 'flag', myfont)
-            
-            pygame.display.flip()
