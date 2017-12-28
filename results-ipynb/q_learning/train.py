c1bf2b45172962ddb6c92cf643f7dc81c9fd0475
A-T-Kristensen <s144026@student.dtu.dk>
2017-12-28 20:41:52 +0100

    Updated ipython notebook with output prints for Q-learning

--- results-ipynb/q_learning/train.py
+++ results-ipynb/q_learning/train.py
@@ -56,84 +56,23 @@ class MinesweeperParameters():
         self.eval_mode = 0
         self.seed = 0
 
-def setup_model(mode = 0):
+def setup_model(mode = "test"):
 
     params = MinesweeperParameters()
 
-    # parser = argparse.ArgumentParser(prog="train.py", description="Train Deep Q-Network for Minesweeper game")
-
-    # # Atari ROM, TensorFlow model and output directory
-    # parser.add_argument('--model', dest='model_file', type=str, required=False, help="path to TensorFlow model file")
-    # parser.add_argument('--out', dest='output_dir', type=str, default="./q_learning/output/", help="output path models and screen captures")
-
-    # parser.add_argument('--train', dest="is_train", action="store_true", help="training or only playing")
-    # #parser.add_argument('--randomstart', dest='random_start_wait', type=int, default=30, help="random number of frames to wait at start of episode")
-    # parser.add_argument('--game', dest='game', type=str, default="DemonAttack-v0", help="The game we play")
-    # parser.add_argument('--env', dest='env', type=str, default="atari", help="If we want to use atari or minesweeper")
-
-    # parser.add_argument('--gpumemory', dest="gpu_memory", type=float, default=0.5, help="The percentage of GPU memory allowed to be used by Tensorflow")
-
-    # # Parameters network input (screens)
-    # parser.add_argument('--inputheight', dest="input_height", type=int, default=84, help="screen input height")
-    # parser.add_argument('--inputwidth', dest="input_width", type=int, default=84, help="screen input width")    
-    # parser.add_argument('--historylength', dest="history_length", type=int, default=4, help="Numbe of moves which are repeated in atari")
-    # parser.add_argument('--mines-min', dest="mines_min", type=int, default=5, help="The number of mines")
-    # parser.add_argument('--mines-max', dest="mines_max", type=int, default=7, help="The number of mines")
-    # parser.add_argument('--nchannels', dest="nchannels", type=int, default=4, help="screen input depth")
-
-    # parser.add_argument('--network-type', dest='network_type', type=int, default=1, help="Different networks")
-
-    # # Parameters CNN architecture
-    # parser.add_argument('--duelingtype', dest="dueling_type", default=None, type=str, help="Type of dueling enabled")
-    # # See 
-    # # http://cs231n.github.io/neural-networks-2/
-    # parser.add_argument('--bias-init', dest="bias_init", type=float, default=0.01, help="The initial value of the biases")
-
-    # # Parameters for training the CNN
-    # parser.add_argument('--num-iterations', dest="num_iterations", type=int, default=50000000, help="Number of training iterations, i.e., number of passes, each pass using [batch size] number of examples")
-    # parser.add_argument('--batchsize', dest="batch_size", type=int, default=32, help="training batch size")
-    # parser.add_argument('--trainfreq', dest="train_freq", type=int, default=4, help="training frequency, default every frame")
-    # parser.add_argument('--epsilonstep', dest="epsilon_step", type=float, default=1e6, help="epsilon decrease step, linear annealing over iterations")
-    # parser.add_argument('--learnrate', dest="learning_rate", type=float, default=0.00025, help="optimization learning rate")
-    # parser.add_argument('--learnratedecay', dest="learning_rate_decay", type=float, default=0.98, help="learning rate decay")
-    # parser.add_argument('--learnratestep', dest="learning_rate_step", type=float, default=100000, help="learning rate decay step over iterations")
-    # parser.add_argument('--learnratemin', dest="learning_rate_minimum", type=float, default=0.0001, help="minimum learning rate")
-    # parser.add_argument('--discount', dest="discount", type=float, default=0.99, help="gamma for future discounted rewards")
-    # parser.add_argument('--clipdelta', dest="clip_delta", type=bool, default=True, help="clipping of error term in loss function")
-    # parser.add_argument('--networkupdate', dest="network_update_rate", type=float, default=10000, help="number of steps after which the Q-network is copied for predicting targets")
-    # parser.add_argument('--batchaccumulator', dest="batch_accumulator", type=str, default="mean", help="batch accumulator in loss function (mean or sum)")
-
-    # parser.add_argument('--replaycap', dest="replay_capacity", type=int, default=int(1e6), help="maximum number of samples in replay memory")
-    # parser.add_argument('--trainstart', dest="train_start", type=int, default=50000, help="start training when replay memory is of this size")
-
-    # # Parameters for evaluation of the model
-    # parser.add_argument('--evalfreq', dest="eval_frequency", type=int, default=250000, help="frequency of model evaluation")
-    # parser.add_argument('--evaliterations', dest="eval_iterations", type=int, default=125000, help="number of games played in each evaluation")
-    # parser.add_argument('--evalepsilon', dest="eval_epsilon", type=float, default=0.05, help="epsilon random move when evaluating")
-    # parser.add_argument('--minepsilon', dest="min_epsilon", type=float, default=0.1, help="Lowest epsilon when exploring")
-    # parser.add_argument('--num-steps', dest="num_steps", type=int, default=5000, help="Number of test steps when playing, each step is an action")
-    # parser.add_argument('--reward-recent-update', dest="reward_recent_update", type=int, default=10000, help="The number of episodes before resetting recent reward")
-    # parser.add_argument('--num-games', dest="num_games", type=int, default=5000, help="Number of test games to play minesweeper")
-
-    # # Parameters for outputting/debugging
-    # parser.add_argument('--intsummary', dest="interval_summary", type=int, default=200, help="frequency of adding training summaries, currently depending on train_iteration")
-    # parser.add_argument('--intcheckpoint', dest="interval_checkpoint", type=int, default=10000, help="frequency of saving model checkpoints")
-    # parser.add_argument('--memorycheckpoint', dest="memory_checkpoint", type=int, default=int(1e5), help="Frequency of saving memory based on addition counter.")
-    # parser.add_argument('--restore-memory', dest="restore_memory", type=bool, default=False, help="If True, restore replay memory.")
-    # parser.add_argument('--show', dest="show_game", action="store_true", help="show the Minesweeper game output")
-    # parser.add_argument('--eval-mode', dest="eval_mode", 
-    #     help="0 = evaluate models in range (only used for selecting the best model), 1 = test model win-rate by playing the game, 2 = win-rate for random mines")
-    # parser.add_argument('--seed', dest="seed", type=int, default=0, help="The random seed value. Default at 0 means deterministic for all ops in Tensorflow 1.4")
-
-    # # Parse command line arguments and run the training process
-
-    if mode == 0: # Train
-        print("Training the network")
+    if mode == "train": # Train
+        print("Training the network which obtained 90.20% win-rate on 6x6 board with 6 mines")
         params.is_train = True
         params.eval_iterations = 1000
         params.eval_frequency=20000
         params.interval_summary=500
 
+        params.memory_checkpoint = int(5e5)
+        params.train_start = int(5e4)
+        params.replay_capacity = int(1e6)
+        params.interval_checkpoint = int(2e4)
+
+        params.batch_size = 400
         params.discount=0.0
         params.learning_rate=0.00025
         params.learning_rate_step=20000
@@ -145,7 +84,34 @@ def setup_model(mode = 0):
 
         run_model(params)
 
-    elif mode == 1: # Test minesweeper
+    elif mode == "train_random_mines": # Train
+        print("Training the network which obtained the best win-rate on 6x6 board with random mines")
+        params.is_train = True
+        params.eval_iterations = 1000
+        params.eval_frequency=20000
+        params.interval_summary=500
+
+        params.mines_min = 1
+        params.mines_max = 12
+
+        params.memory_checkpoint = int(5e5)
+        params.train_start = int(5e4)
+        params.replay_capacity = int(1e6)
+        params.interval_checkpoint = int(2e4)        
+
+        params.batch_size = 400
+        params.discount=0.0
+        params.learning_rate=0.00025
+        params.learning_rate_step=20000
+        params.learning_rate_decay=0.90
+        params.learning_rate_minimum=0.00025/4
+        params.network_update_rate=int(1e5)
+        params.min_epsilon=0.1
+        params.epsilon_step=2.5e5
+
+        run_model(params)
+
+    elif mode == "test": # Test minesweeper
         print("Test minesweeper model on 6x6 board with 6 mines")
         params.output_dir = './q_learning/output_best/'
         params.eval_iterations = 10000
@@ -154,7 +120,7 @@ def setup_model(mode = 0):
 
         run_model(params)
 
-    elif mode == 2: # Evaluate for a different number of mines
+    elif mode == "test_random_mines": # Evaluate for a different number of mines
         print("Test minesweeper model on 6x6 board with a random number of mines")
         params.output_dir = './q_learning/output_best/'
         params.eval_iterations = 10000
@@ -170,7 +136,7 @@ def setup_model(mode = 0):
         tf.reset_default_graph()
         run_model(params)
 
-    elif mode == 3: # Play 10 games
+    elif mode == "play": # Play 10 games
         params.show_game = True
         params.output_dir='./q_learning/output_best'
         params.eval_iterations=10
